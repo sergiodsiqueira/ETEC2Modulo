8f2a0a267c08dd7e5d5ead9fe706a32a2ce5d4fe
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2021-08-10 23:26:02 -0300

    Aula DS 10/08

--- Desenvolvimento de Sistemas/AppCalculoIMC/AppCalculoIMC/Form1.Designer.cs
+++ Desenvolvimento de Sistemas/AppCalculoIMC/AppCalculoIMC/Form1.Designer.cs
@@ -29,64 +29,78 @@ namespace AppCalculoIMC
         /// </summary>
         private void InitializeComponent()
         {
-            this.edtAltura = new System.Windows.Forms.TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.edtPeso = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.edtPeso = new System.Windows.Forms.NumericUpDown();
+            this.edtAltura = new System.Windows.Forms.NumericUpDown();
             this.btnCalcular = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.lblIMC = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.edtPeso)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.edtAltura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // edtAltura
+            // edtPeso
             // 
-            this.edtAltura.Location = new System.Drawing.Point(75, 231);
-            this.edtAltura.Name = "edtAltura";
-            this.edtAltura.Size = new System.Drawing.Size(207, 23);
-            this.edtAltura.TabIndex = 1;
+            this.edtPeso.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.edtPeso.Location = new System.Drawing.Point(66, 53);
+            this.edtPeso.Name = "edtPeso";
+            this.edtPeso.Size = new System.Drawing.Size(93, 23);
+            this.edtPeso.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(146, 18);
+            this.label1.Font = new System.Drawing.Font("Roboto Lt", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(119, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.Size = new System.Drawing.Size(124, 23);
             this.label1.TabIndex = 2;
             this.label1.Text = "Cálculo IMC";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(75, 137);
+            this.label2.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(21, 53);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 15);
+            this.label2.Size = new System.Drawing.Size(39, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Peso:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(75, 213);
+            this.label3.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(182, 53);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.Size = new System.Drawing.Size(71, 15);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Altura:";
+            this.label3.Text = "Altura (CM)";
             // 
-            // edtPeso
+            // edtAltura
             // 
-            this.edtPeso.Location = new System.Drawing.Point(75, 155);
-            this.edtPeso.Name = "edtPeso";
-            this.edtPeso.Size = new System.Drawing.Size(207, 23);
-            this.edtPeso.TabIndex = 5;
+            this.edtAltura.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.edtAltura.Location = new System.Drawing.Point(259, 51);
+            this.edtAltura.Name = "edtAltura";
+            this.edtAltura.Size = new System.Drawing.Size(111, 23);
+            this.edtAltura.TabIndex = 0;
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Location = new System.Drawing.Point(75, 294);
+            this.btnCalcular.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Image = ((System.Drawing.Image)(resources.GetObject("btnCalcular.Image")));
+            this.btnCalcular.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalcular.Location = new System.Drawing.Point(21, 97);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(207, 35);
-            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Size = new System.Drawing.Size(138, 36);
+            this.btnCalcular.TabIndex = 2;
             this.btnCalcular.Text = "&Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
@@ -94,51 +108,108 @@ namespace AppCalculoIMC
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(75, 372);
+            this.label4.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(21, 156);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 15);
+            this.label4.Size = new System.Drawing.Size(90, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Resultado IMC";
             // 
             // lblIMC
             // 
             this.lblIMC.AutoSize = true;
-            this.lblIMC.Location = new System.Drawing.Point(165, 372);
+            this.lblIMC.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblIMC.Location = new System.Drawing.Point(111, 156);
             this.lblIMC.Name = "lblIMC";
-            this.lblIMC.Size = new System.Drawing.Size(12, 15);
+            this.lblIMC.Size = new System.Drawing.Size(13, 15);
             this.lblIMC.TabIndex = 8;
             this.lblIMC.Text = "?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 186);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 352);
+            this.progressBar1.Maximum = 50;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(349, 10);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpar.Image")));
+            this.btnLimpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpar.Location = new System.Drawing.Point(232, 97);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(138, 36);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFechar.Image = ((System.Drawing.Image)(resources.GetObject("btnFechar.Image")));
+            this.btnFechar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFechar.Location = new System.Drawing.Point(21, 397);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(349, 36);
+            this.btnFechar.TabIndex = 11;
+            this.btnFechar.Text = "&Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(393, 441);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblIMC);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.edtPeso);
+            this.Controls.Add(this.edtAltura);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.edtAltura);
+            this.Controls.Add(this.edtPeso);
             this.Name = "Form1";
             this.Text = "Calculo de IMC";
-            ((System.ComponentModel.ISupportInitialize)(this.edtPeso)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.edtAltura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox edtAltura;
+        private System.Windows.Forms.TextBox edtPeso;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown edtPeso;
+        private System.Windows.Forms.NumericUpDown edtAltura;
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
     }
 }
 
--- Desenvolvimento de Sistemas/AppCalculoIMC/AppCalculoIMC/Form1.cs
+++ Desenvolvimento de Sistemas/AppCalculoIMC/AppCalculoIMC/Form1.cs
@@ -21,12 +21,33 @@ namespace AppCalculoIMC
         {
             double peso, altura, imc;
 
-            peso = double.Parse(edtPeso.Text);
-            altura = double.Parse(edtAltura.Text);
+            peso = double.Parse(edtAltura.Text);
+            altura = double.Parse(edtPeso.Text);
 
-            imc = peso/(altura*altura);
+            imc = peso / (altura * altura);
 
             lblIMC.Text = imc.ToString("N2");
+
+            progressBar1.Value = Convert.ToInt32(imc);
+        }
+
+                private void Form1_Load(object sender, EventArgs e)
+                {
+
+                }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            edtAltura.Value = 0;
+            edtPeso.Clear();
+            progressBar1.Value = 0;
+
+            edtAltura.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
