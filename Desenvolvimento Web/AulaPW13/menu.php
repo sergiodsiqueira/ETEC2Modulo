c25a79460377eef24b37e2ceff032fd73046884d
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2021-11-05 20:17:18 -0300

    Aula PW13

---
+++ Desenvolvimento Web/AulaPW13/menu.php
@@ -0,0 +1,7 @@
+    <div class="nav">
+        <ul>
+            <li><a href="index.php"><i class="fas fa-home"></i> Home</a></li>
+            <li><a href="sobre.php"><i class="fas fa-question-circle"></i> Sobre</a></li>
+            <li><a href="login.php"><i class="fas fa-user-lock"></i> Login</a></li>
+        </ul>
+    </div>
